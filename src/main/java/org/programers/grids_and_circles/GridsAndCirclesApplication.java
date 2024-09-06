3d2c13b5d0fde75103e72ffafbd85403da2bcbc8
dowoo kim <dowoo12345@naver.com>
2024-09-06 17:04:42 +0900

    init

---
+++ src/main/java/org/programers/grids_and_circles/GridsAndCirclesApplication.java
@@ -0,0 +1,13 @@
+package org.programers.grids_and_circles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GridsAndCirclesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GridsAndCirclesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/programers/grids_and_circles/GridsAndCirclesApplicationTests.java
@@ -0,0 +1,15 @@
+package org.programers.grids_and_circles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles(value = "test")
+class GridsAndCirclesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
